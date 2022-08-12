49243535e9554888e5f1e89a5ec3389cbb71ba2c
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-13 00:38:38 +0100

    started projects

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import {Flex, Box, Text, Button} from "@chakra-ui/react"
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <h4>Hello sir</h4>
     </div>
   )
 }
