75392b91fbe3247f9c22ccd28b34f2ce000ce229
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-14 13:28:57 +0100

    working on my Navbar

--- components/Layout.js
+++ components/Layout.js
@@ -2,23 +2,27 @@ import Head from "next/head";
 import {Box} from "@chakra-ui/react"
 
 const Layout = ({children}) => {
-            <>
+            return(<>
             <Head>
                 <title>UAE Real Estate Website</title>
             </Head>
 
-            <Box className="w-full bg-neutral-300">
+            <Box className="w-full bg-neutral-200 max-w-full">
              <header>
-                <Navbar />
+                {/* <Navbar /> */}
+                hello
              </header>
-
 
                 <main>
                     {children}
                 </main>
              
+             <footer>
+                {/* <Footer /> */}
+                hdelkkr
+             </footer>
             </Box>
-            </>
+            </>)
 }
 
 export default Layout
