95cca314ae586ba6d752251af7d620bcb6f3a4a4
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-14 02:59:06 +0100

    working on Property component

--- components/Property.js
+++ components/Property.js
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {Flex, Box, Text, Avatar} from "@chakra-ui/react"
 import {FaBed, FaBath } from "react-icons/fa"
 import {GoVerified} from "react-icons/go"
 import { BsGridFill} from "react-icons/bs"
+import {FiPlus, FiMinus} from "react-icons/fi"
 import millify from "millify";
 import defaultImage from "../images/Home.jpg"
 
@@ -13,7 +15,10 @@ const Property = ({property : {
     rentFrequency,rooms, 
     title,externalID,baths,
      area, agency, isVerified
-}}) =>(
+}}) =>{
+
+    const [ isTrue, setisTrue ] = useState(false);
+
     <Link href = {`/property/${externalID}`} passHref>
 
     <Flex flexWrap={"wrap"} w= "400px" p={"5"} paddingTop = {"0"} 
@@ -32,12 +37,24 @@ const Property = ({property : {
 
                 <Text fontWeight={"bold"} fontSize = {"lg"}>AED {millify(price)} { rentFrequency && (`/ ${rentFrequency}`)}</Text>
             </Flex>
+            <Box>
+                <Avatar size={"sm"} src = {agency?.logo?.url}/>
+            </Box>
+
          </Flex>
+         <Flex alignItems = {"center"} p = "1"   justifyContent={"space-between"} w= "250px" color = {"blue.400"}>
+            {rooms} <FaBed /> | {baths} <FaBath /> | {millify (area) } sqt <BsGridFill /> 
+         </Flex>
+
+         <Text fontSize={"lg"} className = {"text-blue-500"}>
+         {title.length > 30 ? ` ${title.substring(0, 35)} ...` : `${title}`}
+         </Text>
+
         </Box>
 
     </Flex>
 
     </Link>
-)
+}
 
 export default Property
