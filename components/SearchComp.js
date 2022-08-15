14f3f2703496c3cec9f46bdbeafeee896a760b1b
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-15 18:32:07 +0100

    working on Select element

--- components/SearchComp.js
+++ components/SearchComp.js
@@ -3,13 +3,32 @@ import { useRouter } from "next/router";
 import { Box, Flex, Select, Input, Spinner, Text,Icon, Button } from "@chakra-ui/react";
 import { FcCancel } from "react-icons/fc";
 import Image from "next/image";
-
+import { filterData, getFilterValues } from "../utils/FilterData";
 const SearchComp = () => { 
+    const [filters, setFilters ] = useState(filterData)
+
+    const PropertiesSearched = ({filterValue}) => {
+     
+    }
+
     return(
-        <Box>
-            Hello
-            <p>Wawa</p>
-        </Box>
+        <Flex bg={"gray.100"} justifyContent = {"center"} flexWrap = "wrap" p={"4"}>
+          {filters.map( (filter) =>(
+            <Box key={filter.queryName}>
+              <Select 
+              p={"2"}
+              placeholder = {filter.placeholder}
+              w = {"fit-content"}
+              onChange={(e)=>PropertiesSearched({[filter.queryName] : e.target.value})}>
+               {
+                filter?.items?.map( (item) => (
+                    <option key={item.name} value={item.name}>{item.name}</option>
+                ))
+               }
+              </Select>
+            </Box>
+          ))}
+        </Flex>
     )
 }
 
--- pages/search.js
+++ pages/search.js
@@ -4,8 +4,9 @@ import Image from "next/image";
 import {Box, Button, Flex, flex, Icon, Text} from "@chakra-ui/react"
 import { BsFilter } from "react-icons/bs";
 import  {Property, SearchComp}  from "../components";
+import { FetchApi, baseUri } from "../utils/FetchApi";
 
-const search = () => {
+const search = ({properties}) => {
     const [searchTerm, setSearchTerm ] = useState(false);
     const Router = useRouter();
 
@@ -34,14 +35,15 @@ const search = () => {
         </Text>
 
         <Flex flexWrap={"wrap"}>
-            {[].map( (property) => <Property property={property} key = {property.id}/>)}
+            {properties.map( (property) => <Property property={property} key = {property.id}/>)}
         </Flex>
 
-        {[].length === 0 &&(
+        {properties.length === 0 &&(
             <Flex justifyContent={"center"} alignItems = {"center"} flexDirection = {"column"}
              marginTop = {"5"} marginBottom = {"5"}
             >
-
+             <h1> Hi Sorry</h1>
+             <h3>No Result Found</h3>
             </Flex>
         )}
 
@@ -50,3 +52,24 @@ const search = () => {
 }
 
 export default search
+
+export async function getServerSideProps({ query }) {
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
+  
+    const data = await FetchApi(`${baseUri}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
+  
+    return {
+      props: {
+        properties: data?.hits,
+      },
+    };
+  }
