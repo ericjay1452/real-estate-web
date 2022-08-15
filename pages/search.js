a4a0fd3f95447c211ca0c5999b9ac7d32a0ff97d
Ilesanmi-Erioluwa <codeineboy1452@gmail.com>
2022-08-15 15:42:09 +0100

    Started working on search page

---
+++ pages/search.js
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import {Box, flex, Icon, Text} from "@chakra-ui/react"
+import { BsSearch } from "react-icons/bs";
+
